41fb312e9e84ba1ce6314a5264867c4751e5d65d
mg699 <mg699@kent.ac.uk>
2022-03-27 21:05:39 +0100

    Formatted things a bit better

--- main.erl
+++ main.erl
@@ -7,9 +7,7 @@ empty_tree() ->
         {is_empty, PID} -> PID ! true, empty_tree();
         {get, _, PID} -> PID ! nothing, empty_tree();
         {put, K, V, PID} -> PID ! done, binary_tree(K, V, empty(), empty());
-        {fold, FE, _, PID} ->
-            PID ! {folded, FE, self()},
-            empty_tree()
+        {fold, FE, _, PID} -> PID ! {folded, FE, self()}, empty_tree()
     end.
 
 binary_tree(KK, VV, L, R) ->
@@ -17,14 +15,12 @@ binary_tree(KK, VV, L, R) ->
     receive
 
         {is_empty, PID} -> PID ! false, Repeat();
-
         {get, K, PID} ->
             if
                 K < KK -> L ! {get, K, PID}, Repeat(); %% search left
                 K > KK -> R ! {get, K, PID}, Repeat(); %% search right
                 true -> PID ! {just, VV}, Repeat() %% found K
             end;
-
         {put, K, V, PID} ->
             PID ! done,
             if
@@ -32,7 +28,6 @@ binary_tree(KK, VV, L, R) ->
                 K > KK -> R ! {put, K, V, PID}, Repeat(); %% go right
                 true -> binary_tree(K, V, L, R) %% replace node K/V
             end;
-
         {fold, FE, FB, PID} ->
             L ! {fold, FE, FB, self()},
             LV = receive {folded, RES, _} -> RES end,
@@ -41,36 +36,26 @@ binary_tree(KK, VV, L, R) ->
             RV = receive {folded, RES2, _} -> RES2 end,
             
             PID ! {folded, FB(LV, KK, VV, RV), self()},
-
             binary_tree(KK, VV, L, R)
     end.
 
-empty() ->
-    spawn(?MODULE, empty_tree, []).
+empty() -> spawn(?MODULE, empty_tree, []).
 
 get(T, K) ->
     T ! {get, K, self()},
-    receive
-        RESP -> RESP
-    end.
+    receive RESP -> RESP end.
 
 put(T, K, V) ->
     T ! {put, K, V, self()},
-    receive
-        RESP -> RESP
-    end.
+    receive RESP -> RESP end.
 
 is_empty(T) ->
     T ! {is_empty, self()},
-    receive
-        RESP -> RESP
-    end.
+    receive RESP -> RESP end.
 
 fold(T, FE, FB) ->
     T ! {fold, FE, FB, self()},
-    receive
-        {folded, RES, _} -> RES
-    end.
+    receive {folded, RES, _} -> RES end.
 
 %% Testing
 
