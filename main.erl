5bac92d5161ec1f069870e7ea2e5153515650f14
mg699 <mg699@kent.ac.uk>
2022-03-27 21:04:20 +0100

    Added user-friendly functions

--- main.erl
+++ main.erl
@@ -2,23 +2,6 @@
 -compile(export_all).
 -compile(nowarn_export_all).
 
-%% implement binary trees
-%% every node is a process, even empty trees
-
-%% two kinds of tree processes:
-%% empty trees and binary tree nodes
-
-% FOR REFERENCE ----
-%
-% compGH(F, G) ->
-%     receive
-%         PID -> PID ! fun(I) -> F(G(I)) end, compGH(F, G)
-%     end.
-
-% compG(F, G) ->
-%     PID = spawn(?MODULE, compGH, [F, G]),
-%     fun(I) -> PID ! self(), receive FG -> FG(I) end end.
-
 empty_tree() ->
     receive
         {is_empty, PID} -> PID ! true, empty_tree();
@@ -65,64 +48,31 @@ binary_tree(KK, VV, L, R) ->
 empty() ->
     spawn(?MODULE, empty_tree, []).
 
-%% Test functions
-
-flush() ->
+get(T, K) ->
+    T ! {get, K, self()},
     receive
-            X -> erlang:display(X)
+        RESP -> RESP
     end.
 
-testTree(X) ->
-    X ! {put, d, 5, self()},
-    flush(),
-    X ! {put, b, 17, self()},
-    flush(),
-    X ! {put, h, 19, self()},
-    flush(),
-    X ! {put, c, 12, self()},
-    flush().
-
-checkempty(PID) ->
-    PID ! {is_empty, self()},
+put(T, K, V) ->
+    T ! {put, K, V, self()},
     receive
-        X -> X
+        RESP -> RESP
     end.
 
-% test2(PID) ->
-%     PID ! {put, a, 12, self()},
-%     receive
-%         X -> X
-%     end.
-
-% test3(PID) ->
-%     PID ! {get, a, self()},
-%     receive
-%         X -> X
-%     end.
-
-getb(PID) ->
-    PID ! {get, b, self()},
+is_empty(T) ->
+    T ! {is_empty, self()},
     receive
-        X -> X
+        RESP -> RESP
     end.
 
-foldtest(PID) ->
-    FB = fun(L1, _,V,L2) -> L1 + L2 + V end,
-    PID ! {fold, 0, FB, self()},
+fold(T, FE, FB) ->
+    T ! {fold, FE, FB, self()},
     receive
-        X -> X
+        {folded, RES, _} -> RES
     end.
 
-%% For testing
-%%
-%% X = main:empty().
-%% X ! {is_empty, self()}, receive L->L end.
-%% X ! {put, a, 120, self()}, receive L->L end.
-%% X ! {get, a, self()}, receive L->L end.
-%% X ! {get, b, self()}, receive L->L end.
-%%
-%% spawn(main, binary_tree, [a, 12, main:empty(), main:empty()]).     
-%% <0.76.0> ! {get, a, self()}, receive L->L end.
-%%
-%% X = main:empty().
-%% X ! {put, b, 21, self()}, receive L->L end.
+%% Testing
+
+%% X=main:empty(), main:put(X,d,5), main:put(X,b,17), main:put(X,h,19), main:put(X,c,12).
+%% main:fold(X,0,fun(L,_,V,R)->L+V+R end).
